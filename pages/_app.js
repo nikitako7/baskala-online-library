19e40d93f1770b2b16bffc4706021a2a0aefb5d1
yihnatchuk <yihnatchuk@griddynamics.com>
2022-11-19 23:24:14 +0200

    feature/intro

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import '../styles/globals.css'
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
