ff5ef41f6d87c771efe6a1535f07fd79280b34ec
yihnatchuk <yihnatchuk@griddynamics.com>
2022-11-20 19:47:28 +0200

    feature/swiper

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-import { createClient } from 'contentful'; 
-import Book from '../components/Book';
+import { createClient } from 'contentful';
 import { Intro } from '../components/Intro/Intro';
+import Book from '../components/Book';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
 
 export async function getStaticProps(params) {
   const client = createClient({
@@ -17,15 +20,25 @@ export async function getStaticProps(params) {
   }
 }
 
-export default function Books({ books  }) {
+export default function Books({ books }) {
   return (
     <>
       <Intro />
       <div className="page-content">
         <div className="book-list">
-          {books.map( (book)  => (
-            <Book key={book.sys.id} book={book} />
-          ))}
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={3}
+            loop
+            pagination={{ clickable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+          >
+            {books.map((book) => (
+              <SwiperSlide key={book.sys.id}>
+                <Book book={book} />
+              </SwiperSlide>))}
+          </Swiper>
         </div>
       </div>
     </>
