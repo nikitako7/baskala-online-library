05672d7989dab20461be82d47bb28eb3b8fabaed
mkonovalov <mkonovalov@griddynamics.com>
2022-11-24 20:40:06 +0200

    add author book slider

--- pages/authors/[slug].js
+++ pages/authors/[slug].js
@@ -2,8 +2,15 @@ import { createClient } from 'contentful';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import Image from 'next/image';
 import NotFound from '../404';
+import Book from '../../components/Book/Book';
 import styles from './author-slug.module.scss';
-import Slider from '../../components/Slider/Slider';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation } from 'swiper';
+import "swiper/components/navigation/navigation.min.css";
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
+
+SwiperCore.use([Navigation]);
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -33,6 +40,8 @@ export const getStaticProps = async ({ params }) => {
     'fields.slug': params.slug
   });
 
+  const res =  await client.getEntries({ content_type: 'book' });
+
   if (!items.length) {
     return {
       redirect: {
@@ -43,35 +52,58 @@ export const getStaticProps = async ({ params }) => {
   }
 
   return {
-    props: { author: items[0] },
+    props: { author: items[0], books: res.items },
   }
 
 }
 
-export default function BookDetails({ author }) {
+export default function BookDetails({ author, books }) {
   if (!author) return <NotFound />;
 
   const { fullName, photo, description } = author.fields;
 
   return (
-    <div className={styles.book}>
-      <div className={styles.book__leftside}>
-        <div className={styles.book__leftside_imageWrapper}>
-          <Image 
-            src={'https:' + photo.fields.file.url}
-            width={photo.fields.file.details.image.width}
-            height={photo.fields.file.details.image.height}
-            className={styles.book__leftside_image}
-          />
+    <>
+        <div className={styles.book}>
+            <div className={styles.book__leftside}>
+                <div className={styles.book__leftside_imageWrapper}>
+                    <Image 
+                        src={'https:' + photo.fields.file.url}
+                        width={photo.fields.file.details.image.width}
+                        height={photo.fields.file.details.image.height}
+                        className={styles.book__leftside_image}
+                    />
+                </div>
+            </div>
+            <div className={styles.book__rightside}>
+                <h2 className={styles.book__rightside_title}>{ fullName }</h2>
+                <h3 className={styles.book__rightside_descriptionTitle}>About This Author</h3>
+                <div className={styles.book__rightside_description}>{documentToReactComponents(description)}</div>
+            </div>
         </div>
-      </div>
-      <div className={styles.book__rightside}>
-          <h2 className={styles.book__rightside_title}>{ fullName }</h2>
-          <h3 className={styles.book__rightside_descriptionTitle}>About This Author</h3>
-          <div className={styles.book__rightside_description}>{documentToReactComponents(description)}</div>
-          <Slider />
-      </div>
-    </div>
-    
+        <div className="page-content">
+            <div className="book-list">
+                <h3 className='label'>Книги автора</h3>
+                <Swiper
+                className='list'
+                spaceBetween={50}
+                slidesPerView={5}
+                navigation={true}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+                >
+                {books.map((book) => {
+                    if (fullName == book.fields.author.fields.fullName) {
+                        return (
+                            <SwiperSlide key={author.sys.id}>
+                            <Book book={book} />
+                            </SwiperSlide>)
+                    }
+                    return;
+                })}
+                </Swiper>
+            </div>
+        </div>
+    </>
   )
 }
