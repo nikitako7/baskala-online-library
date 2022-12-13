9ad417329e26a9e89d5ca51f1a1c1ab6e9f7da1b
mkonovalov <mkonovalov@griddynamics.com>
2022-12-13 19:51:26 +0200

    create modal localization buttons

--- components/Modal/Modal.js
+++ components/Modal/Modal.js
@@ -2,29 +2,38 @@ import React from 'react';
 import { useRouter } from 'next/router'
 import { links, options } from '../../utils/index';
 import styles from './Modal.module.scss';
+import { languageSelector, setLanguage } from '../../store/languageSlice';
+import { useDispatch, useSelector } from "react-redux";
 
 export const Modal = ({ closeModal }) => {
   const router = useRouter();
+  const dispatch = useDispatch();
+  const curLanguage = useSelector(languageSelector);
 
   const onClickHandler = (path) => {
+    const curTitle = (curLanguage === 'tt' && title) || (curLanguage === 'ru' && titleRu) || (curLanguage === 'tt-lt' && titleTtlt);
+    setActiveLink({id, curTitle});
     closeModal(false);
     path && router.push(path);
   };
 
   return (
     <div className={styles.modal}>
+        <h4 className={styles.modal__title}>{ (curLanguage === 'tt' && 'Телне сайлагыз') || (curLanguage === 'ru' && 'Выбрать язык') || (curLanguage === 'tt-lt' && 'Select Language') }</h4>
         <nav className={styles.modal__links}>
           <ul>
-            {links.map(({ id, title, path }) => (
+            {links.map(({ id, title, titleRu, titleTtlt, path }) => (
               <li key={id} onClick={() => onClickHandler(path)}>
-                { title }
+                { (curLanguage === 'tt' && title) || (curLanguage === 'ru' && titleRu) || (curLanguage === 'tt-lt' && titleTtlt) }
               </li>
             ))}
           </ul>
           <div className={styles.modal__select}>
             <div>
               {options.map(({ value, label }) => (
-                <span key={value}>{ label }</span>
+                <button className={styles.btn} key={value} value={label} onClick={(e) => {
+                  return dispatch(setLanguage(e.target.value));
+              }}>{ label }</button>
               ))}
             </div>
           </div>
